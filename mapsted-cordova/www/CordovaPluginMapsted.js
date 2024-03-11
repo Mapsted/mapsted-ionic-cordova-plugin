4eac5dd4949e4e0caa0e944b6522ecb6b3e94fb4
Sakshivijayvargiya <sakshi.vijayvargiya@swapinfotech.com>
2024-03-11 15:20:26 +0530

    Modification done cordova plugin folder

---
+++ mapsted-cordova/www/CordovaPluginMapsted.js
@@ -0,0 +1,27 @@
+var exec = require("cordova/exec");
+
+var PLUGIN_NAME = "CordovaPluginMapsted";
+
+exports.coolMethod = function (arg0, success, error) {
+  exec(success, error, PLUGIN_NAME, "coolMethod", [arg0]);
+};
+
+module.exports.echo = function (arg0, success, error) {
+  exec(success, error, PLUGIN_NAME, "echo", [arg0]);
+};
+
+module.exports.launchMapActivity = function (success, error) {
+  exec(success, error, PLUGIN_NAME, "launchMapActivity", []);
+};
+
+module.exports.getSearchCoreSdkCallback = function (success, error) {
+  exec(success, error, PLUGIN_NAME, "getSearchCoreSdkCallback", []);
+};
+
+module.exports.getSearchFeedCallback = function (success, error) {
+  exec(success, error, PLUGIN_NAME, "getSearchFeedCallback", []);
+};
+
+module.exports.getSearchAlertCallback = function (success, error) {
+  exec(success, error, PLUGIN_NAME, "getSearchAlertCallback", []);
+};
